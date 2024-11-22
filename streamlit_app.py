e3b1c91e15a0329fad52151babf2415481a00fab
Black Box Research <drummerben@me.com>
2024-11-22 13:14:18 +0000

    added side bar promo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,63 @@
 import streamlit as st
 from streamlit_extras.stylable_container import stylable_container
 
-st.logo(image="static/bbs_type_logo.png", icon_image="static/bbs-icon.png", size="large")
+st.logo(image="static/bbs_type_logo.png", size="large")
 
 st.set_page_config(layout="centered")
 
 def dashboard_page():
-    with st.container(border=True):
-        st.title("Welcome, Ben!", anchor=False)
+    with st.container(border=False):
+        st.markdown(
+                    """
+                    <h3 style="
+                        font-size: 2em;
+                        background: linear-gradient(90deg, #CDFFD8, #94B9FF);
+                        -webkit-background-clip: text;
+                        -webkit-text-fill-color: transparent;
+                        font-weight: bold;
+                        text-align: left;">
+                        Welcome, Ben!
+                    </h3>
+                    """,
+                    unsafe_allow_html=True
+                )
 
 def systems_page():
-    st.title("Systems", anchor=False)
+    with st.container(border=False):
+        st.markdown(
+                    """
+                    <h3 style="
+                        font-size: 2em;
+                        background: linear-gradient(90deg, #CDFFD8, #94B9FF);
+                        -webkit-background-clip: text;
+                        -webkit-text-fill-color: transparent;
+                        font-weight: bold;
+                        text-align: left;">
+                        Systems
+                    </h3>
+                    """,
+                    unsafe_allow_html=True
+                )
 
 def settings_page():
-    st.title("Settings", anchor=False)
+    with st.container(border=False):
+        st.markdown(
+                    """
+                    <h3 style="
+                        font-size: 2em;
+                        background: linear-gradient(90deg, #CDFFD8, #94B9FF);
+                        -webkit-background-clip: text;
+                        -webkit-text-fill-color: transparent;
+                        font-weight: bold;
+                        text-align: left;">
+                        Settings
+                    </h3>
+                    """,
+                    unsafe_allow_html=True
+                )
 
 def logout_page():
-    st.title("Logout", anchor=False)
+    st.header("Logout", anchor=False)
 
 # Define pages as a dictionary of page groups and individual pages
 pages = {
@@ -29,6 +70,58 @@ pages = {
         st.Page(logout_page, title="Logout", icon=":material/logout:"),
     ],
 }
+
+with st.sidebar:
+    with st.container(border=True, height=240):
+        st.markdown(
+            """
+            <h3 style="
+                font-size: 1.5em;
+                font-weight: bold;
+                text-align: left;">
+                Upgrade to pro for Life
+            </h3>
+            """,
+            unsafe_allow_html=True
+        )
+        st.markdown(
+            """
+            <h3 style="
+                font-size: 1em;
+                background: linear-gradient(90deg, #CDFFD8, #94B9FF);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                font-weight: bold;
+                text-align: left;">
+                Limited lifetime deal ends soon
+            </h3>
+            """,
+            unsafe_allow_html=True
+        )
+        st.caption("After this exclusive early access deal, we are switching to monthly/annual pricing.")
+        # Button with a link using st.markdown
+        st.markdown(
+            """
+            <div style="text-align: center;">
+                <a href="https://pay.analytiq.trade/b/test_6oE0043QndEzbLi5kk" 
+                    target="_blank" 
+                    style="
+                        display: inline-block; 
+                        padding: 0.5em 1em; 
+                        background-image: linear-gradient(90deg, #CDFFD8, #94B9FF); 
+                        color: #111111; 
+                        text-decoration: none; 
+                        font-size: 0.85em;
+                        border-radius: 0.5rem;
+                        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+                    ">
+                    ✨ Upgrade Now
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
 
 # Set up the navigation
 pg = st.navigation(pages, position="sidebar")
