40b11e43324c5afb9f78b46fa5c1f6dd661e5c8c
Black Box Research <drummerben@me.com>
2024-11-28 10:50:29 +0000

    removed theming

--- streamlit_app.py
+++ streamlit_app.py
@@ -921,42 +921,6 @@ with st.sidebar:
 
         st.markdown("")
     
-    ms = st.session_state
-    if "themes" not in ms: 
-        ms.themes = {"current_theme": "light",
-                            "refreshed": True,
-                            
-                            "dark": {"theme.base": "dark",
-                                    "theme.backgroundColor": "#111111",
-                                    "theme.primaryColor": "#94B9FF",
-                                    "theme.secondaryBackgroundColor": "#171717",
-                                    "theme.textColor": "#e8e8e8",
-                                    "button_face": ":material/light_mode:"},
-
-                            "light":  {"theme.base": "light",
-                                    "theme.primaryColor": "#94B9FF",
-                                    "theme.textColor": "#111111",
-                                    "button_face": ":material/light_mode:"},
-                            }
-
-    def ChangeTheme():
-        previous_theme = ms.themes["current_theme"]
-        tdict = ms.themes["light"] if ms.themes["current_theme"] == "light" else ms.themes["dark"]
-        for vkey, vval in tdict.items(): 
-            if vkey.startswith("theme"): st._config.set_option(vkey, vval)
-
-        ms.themes["refreshed"] = False
-        if previous_theme == "dark": ms.themes["current_theme"] = "light"
-        elif previous_theme == "light": ms.themes["current_theme"] = "dark"
-
-    btn_face = ms.themes["light"]["button_face"] if ms.themes["current_theme"] == "light" else ms.themes["dark"]["button_face"]
-    st.button(btn_face, on_click=ChangeTheme)
-
-    if ms.themes["refreshed"] == False:
-        ms.themes["refreshed"] = True
-    st.rerun()
-
-
     with bottom():
         social_media_links = [
             "https://x.com/blackboxstats",
